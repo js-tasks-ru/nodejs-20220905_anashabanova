626e59016c64bef60eb12da5c3216c0bed4699f3
AnastasiyaSN <sh.anastasiya.nick@yandex.ru>
2022-10-06 00:03:28 +0300

    Added tasks of 03-streams section 3-5

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,18 +1,69 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
+
+const endWithErrorCode = (res, code) => {
+  res.statusCode = code;
+  res.end();
+};
+
+const getFileAndWriteIt = (filepath, req, res) => {
+  const limitedStream = new LimitSizeStream({limit: 1000000});
+  const outStream = fs.createWriteStream(filepath);
+  req.pipe(limitedStream).pipe(outStream);
+
+  const abortProcess = () => {
+    outStream.destroy();
+    limitedStream.destroy();
+    fs.unlink(filepath, () => {});
+  };
+
+  req.on('end', () => {
+    limitedStream.end();
+  });
+  req.on('aborted', () => {
+    abortProcess();
+  });
+
+  limitedStream.on('error', (err) => {
+    if (err.code === 'LIMIT_EXCEEDED') {
+      endWithErrorCode(res, 413);
+      abortProcess();
+      return;
+    }
+    endWithErrorCode(res, 500);
+  });
+  limitedStream.on('end', () => {
+    endWithErrorCode(res, 201);
+  });
+};
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const parsedPath = path.parse(pathname);
+
+  const isDirExist = !!parsedPath.dir;
+  if (isDirExist) {
+    endWithErrorCode(res, 400);
+    return;
+  }
 
   switch (req.method) {
     case 'POST':
-
+      const filepath = path.join(__dirname, 'files', pathname);
+      fs.access(filepath, fs.F_OK, (err) => {
+        if (err) {
+          getFileAndWriteIt(filepath, req, res);
+          return;
+        }
+        endWithErrorCode(res, 409);
+        return;
+      });
       break;
 
     default:
--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,11 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
+const endWithErrorCode = (res, code) => {
+  res.statusCode = code;
+  res.end();
+};
 
 const server = new http.Server();
 
@@ -8,11 +13,29 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  const parsedPath = path.parse(pathname);
+
+  const isDirExist = !!parsedPath.dir;
+  if (isDirExist) {
+    endWithErrorCode(res, 400);
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
-
+      if (!fs.existsSync(filepath)) {
+        endWithErrorCode(res, 404);
+        return;
+      }
+      fs.unlink(filepath, function(err) {
+        if (err) {
+          endWithErrorCode(res, 500);
+          return;
+        }
+        endWithErrorCode(res, 200);
+      });
       break;
 
     default:
